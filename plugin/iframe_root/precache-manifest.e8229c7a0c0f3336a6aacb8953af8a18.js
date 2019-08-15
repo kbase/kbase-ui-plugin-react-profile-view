f8922310a8c8bcd43390c9ac5f910b03c4a9ff1b
Erik Pearson <eapearson@lbl.gov>
2019-08-15 11:37:48 -0700

    include a prebuilt plugin - this is the first step to resolve kbase-ui build issues in travis - an addition change will be implemented to conduct these builds at ... travis.

---
+++ plugin/iframe_root/precache-manifest.e8229c7a0c0f3336a6aacb8953af8a18.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0436fc47cfb9980598ae7a27acb7d9bb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9381557bf1fdc38e5234",
+    "url": "./static/css/2.a4e97285.chunk.css"
+  },
+  {
+    "revision": "c2ef93b85557a08721c7",
+    "url": "./static/css/main.d2230bbb.chunk.css"
+  },
+  {
+    "revision": "9381557bf1fdc38e5234",
+    "url": "./static/js/2.48d20184.chunk.js"
+  },
+  {
+    "revision": "c2ef93b85557a08721c7",
+    "url": "./static/js/main.b962aba3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "154d7698e565de3d15be795591fff795",
+    "url": "./static/media/Oxygen-Bold-webfont.154d7698.ttf"
+  },
+  {
+    "revision": "42b65e856b1c1483cf8bb4c819a910a6",
+    "url": "./static/media/Oxygen-Bold-webfont.42b65e85.eot"
+  },
+  {
+    "revision": "8e0eebd63abc520166921d98309acdfb",
+    "url": "./static/media/Oxygen-Bold-webfont.8e0eebd6.woff"
+  },
+  {
+    "revision": "c3968d44dd2a5dc13c823c339e816016",
+    "url": "./static/media/Oxygen-Bold-webfont.c3968d44.svg"
+  },
+  {
+    "revision": "9804ed08f5031e43d2339e352739cfd0",
+    "url": "./static/media/Oxygen-BoldItalic-webfont.9804ed08.ttf"
+  },
+  {
+    "revision": "adc31d51c6e2c8794f727f0cdd07d4df",
+    "url": "./static/media/Oxygen-BoldItalic-webfont.adc31d51.svg"
+  },
+  {
+    "revision": "addd38e737fd7ce16974a2b96eebab3a",
+    "url": "./static/media/Oxygen-BoldItalic-webfont.addd38e7.eot"
+  },
+  {
+    "revision": "b7a5d7e42790d3d1b68bd0c7cbe7d084",
+    "url": "./static/media/Oxygen-BoldItalic-webfont.b7a5d7e4.woff"
+  },
+  {
+    "revision": "29dab8e5ec5d745c23f3ebbd1f7c90fb",
+    "url": "./static/media/Oxygen-Italic-webfont.29dab8e5.svg"
+  },
+  {
+    "revision": "6361dfb49a1cdbbe7c599475a31b5107",
+    "url": "./static/media/Oxygen-Italic-webfont.6361dfb4.eot"
+  },
+  {
+    "revision": "bc98f5b3ba96944f3c24052cf2985922",
+    "url": "./static/media/Oxygen-Italic-webfont.bc98f5b3.ttf"
+  },
+  {
+    "revision": "fd308e0c55ce22312cdecd7ead38df1a",
+    "url": "./static/media/Oxygen-Italic-webfont.fd308e0c.woff"
+  },
+  {
+    "revision": "084df6d2ec008b96a037dffd955bffe8",
+    "url": "./static/media/Oxygen-webfont.084df6d2.eot"
+  },
+  {
+    "revision": "8cde84ea55337d7d12aeabb252373795",
+    "url": "./static/media/Oxygen-webfont.8cde84ea.ttf"
+  },
+  {
+    "revision": "aeebfc548063c12cabe6e930cd9a3021",
+    "url": "./static/media/Oxygen-webfont.aeebfc54.svg"
+  },
+  {
+    "revision": "b8d4397b7c13795c75d3746a21cc1d00",
+    "url": "./static/media/Oxygen-webfont.b8d4397b.woff"
+  },
+  {
+    "revision": "0d38e422650542663242d2ef6d870bf1",
+    "url": "./static/media/OxygenMono-Regular-webfont.0d38e422.eot"
+  },
+  {
+    "revision": "139a0b56c3ee174efad4bb9983fdd64a",
+    "url": "./static/media/OxygenMono-Regular-webfont.139a0b56.woff"
+  },
+  {
+    "revision": "292dcbf9a67b25b5a779cc738f5192cd",
+    "url": "./static/media/OxygenMono-Regular-webfont.292dcbf9.ttf"
+  },
+  {
+    "revision": "d992c3c55ef716a2efff807066ef31cf",
+    "url": "./static/media/OxygenMono-Regular-webfont.d992c3c5.svg"
+  },
+  {
+    "revision": "31d83e664997e947e3b9089f4564fcfe",
+    "url": "./static/media/Roboto-Black-webfont.31d83e66.svg"
+  },
+  {
+    "revision": "98db2e4cb3a052980d02ff5a410f1870",
+    "url": "./static/media/Roboto-Black-webfont.98db2e4c.eot"
+  },
+  {
+    "revision": "bb4b599520aa3bbd0e355fd089fcbebd",
+    "url": "./static/media/Roboto-Black-webfont.bb4b5995.woff"
+  },
+  {
+    "revision": "dda748416dd3e27e9644ec59f37bf751",
+    "url": "./static/media/Roboto-Black-webfont.dda74841.ttf"
+  },
+  {
+    "revision": "9af42ba936d0330131e658179a2d0d77",
+    "url": "./static/media/nouserpic.9af42ba9.png"
+  }
+]);
